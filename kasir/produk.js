a45c460c0f2d974ed02c2c02464c319a0a554886
Riki Koernia <126800106+rikisaja@users.noreply.github.com>
2023-07-04 16:50:39 +0700

    Add files via upload

---
+++ kasir/produk.js
@@ -0,0 +1,22 @@
+var data_produk = [
+    {
+        "foto": "aset/produk/product_capuchino.jpg",
+        "nama": "Cappuccino",
+        "harga": 30000
+    },
+    {
+        "foto": "aset/produk/product_espresso.jpg",
+        "nama": "Espresso",
+        "harga": 20000
+    },
+    {
+        "foto": "aset/produk/product_latte.jpg",
+        "nama": "Latte",
+        "harga": 25000
+    },
+    {
+        "foto": "aset/produk/product_kopi_susu.jpg",
+        "nama": "Kopi Susu",
+        "harga": 35000,
+    }
+]
